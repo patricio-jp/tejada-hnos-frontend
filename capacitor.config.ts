42a93a35d2030872cc3714e95bb3d66bdd92019d
patricio-jp <patojimenezpaz@gmail.com>
2025-09-24 12:27:07 -0300

    Add Capacitor with Android and iOS targets

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tejada.app',
+  appName: 'tejada-front',
+  webDir: 'dist'
+};
+
+export default config;
