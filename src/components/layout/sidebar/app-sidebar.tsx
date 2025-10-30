7974a4b10d3b6239e7ebeff49388e3e9b039d02d
Nahu333 <nahuelpcollado@gmail.com>
2025-10-30 04:49:31 -0300

    feat(layout): mostrar sesión en el sidebar

--- src/components/layout/sidebar/app-sidebar.tsx
+++ src/components/layout/sidebar/app-sidebar.tsx
@@ -2,29 +2,20 @@
 
 import * as React from "react"
 
-import { NavMain } from "@/components/layout/sidebar/nav-main"
-import { NavUser } from "@/components/layout/sidebar/nav-user"
+import { NavMain } from '@/components/layout/sidebar/nav-main'
+import { NavUser } from '@/components/layout/sidebar/nav-user'
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
   SidebarRail,
-} from "@/components/ui/sidebar"
-import { SystemInfo } from "./system-info"
-import { SystemHeader } from "./system-header"
-import { MENUS_MAIN } from "@/common/consts/menus"
+} from '@/components/ui/sidebar'
+import { SystemInfo } from './system-info'
+import { SystemHeader } from './system-header'
+import { MENUS_MAIN } from '@/common/consts/menus'
 
-// This is sample data.
-const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  }
-}
-
-const MENU_LINKS = MENUS_MAIN;
+const MENU_LINKS = MENUS_MAIN
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
@@ -36,7 +27,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <NavMain items={MENU_LINKS} />
       </SidebarContent>
       <SidebarFooter>
-        <NavUser user={data.user} />
+        <NavUser />
         <SystemInfo />
       </SidebarFooter>
       <SidebarRail />
