76857e9fff09453479f15eb15269a2d80b1c0095
patricio-jp <patojimenezpaz@gmail.com>
2025-11-04 20:52:01 -0300

    Add auto-redirect on /login page

--- src/modules/Auth/pages/login.tsx
+++ src/modules/Auth/pages/login.tsx
@@ -1,7 +1,16 @@
 import { LoginForm } from "@/components/login-form"
 import { ThemeProvider } from "@/lib/theme"
+import useAuth from "@/modules/Auth/hooks/useAuth"
+import { Navigate } from "react-router"
 
 export default function LoginPage() {
+  const { isAuthenticated } = useAuth()
+
+  // Si el usuario ya está autenticado, redirigir a la página principal
+  if (isAuthenticated) {
+    return <Navigate to="/" replace />
+  }
+
   return (
     <ThemeProvider defaultTheme="system" storageKey="ui-theme">
       <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
